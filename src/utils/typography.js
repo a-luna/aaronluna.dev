c959bee69931237b676cffa7deab61a2e5cb76d3
Aaron Luna <aaron@alunablog.com>
2019-09-04 08:46:34 -0700

    removing gatsby files, will migrate hugo blog

--- src/utils/typography.js
+++
@@ -1,5 +0,0 @@
-import Typography from "typography"
-import noriegaTheme from "typography-theme-noriega"
-const typography = new Typography(noriegaTheme)
-export const { scale, rhythm, options } = typography
-export default typography
