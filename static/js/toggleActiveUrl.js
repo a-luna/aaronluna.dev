02ed9836aec650ea0570d42dd039af30e260b0a6
Aaron Luna <aaron@alunablog.com>
2019-09-18 20:44:30 -0700

    more tweaks

--- static/js/toggleActiveUrl.js
+++ static/js/toggleActiveUrl.js
@@ -1,9 +1,8 @@
 (function() {
-  const toggleActive = function(link) {
-    const link_url = link.attributes.href.value;
-    if (location.href.indexOf(link_url) > 0) {
-      link.classList.add("active");
-    }
-  };
-  document.querySelectorAll('.site-menu-link').forEach(link => toggleActive(link));
+    const toggleActive = function(link) {
+        if (location.href == link.href) {
+            link.classList.add("active");
+        }
+    };
+    document.querySelectorAll('.site-menu-link').forEach(link => toggleActive(link));
 }())
