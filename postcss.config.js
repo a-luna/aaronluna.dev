ea6cd7b8e67b491bd07b1af5709e7030f08066f2
Aaron Luna <aaron@alunablog.com>
2019-10-09 01:07:32 -0700

    fixed uncss, no longer removes js classes

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,6 @@ module.exports = {
     plugins: [
         require('autoprefixer')({
             grid: true,
-        }),
-        require('postcss-uncss')({
-            html: ['public/**/*.html'],
-            whitelist: ['#menu a.active']
         })
-    ]
     ]
 }
