3365139e58e007babcd3f13aea87c547fcf38590
Aaron Luna <aaron@alunablog.com>
2019-10-08 23:02:13 -0700

    postcss again

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,6 @@ module.exports = {
     plugins: [
         require('autoprefixer')({
             grid: true,
-        }),
-        require('postcss-uncss')({
-            html: ['./public/**/*.html']
         })
     ]
 }
