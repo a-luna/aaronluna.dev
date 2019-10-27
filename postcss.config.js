7537556a405196fb168c8a276e4d379b2b2c57ae
Aaron Luna <aaron@alunablog.com>
2019-10-27 14:49:58 -0700

    updated postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,10 @@ module.exports = {
     plugins: [
         require('autoprefixer')({
             grid: true,
+        }),
+        require('postcss-uncss')({
+            html: ['public/**/*.html'],
+            whitelist: ['#menu a.active']
         })
     ]
 }
