4a958f224856db18bd79fe44f9d318e46887aad1
Aaron Luna <aaron@alunablog.com>
2020-01-08 15:08:55 -0800

    changing to async/await syntax...

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,10 @@
-const CACHE_VERSION = 8;
+const CACHE_VERSION = 9;
 const CACHE_NAME = `content-v${CACHE_VERSION}`;
+const OFFLINE_PAGE = "/offline/index.html";
+const NOT_FOUND_PAGE = "/404.html";
+const SUPPORTED_METHODS = ["GET"];
+const SUPPORTED_URL_SCHEMES = ["http:", "https:"];
+
 const CACHE_FILES = [
   "/offline/index.html",
   "/404.html",
@@ -8,10 +13,6 @@ const CACHE_FILES = [
   "/manifest.json"
 ];
 
-const OFFLINE_PAGE = "/offline/index.html";
-const NOT_FOUND_PAGE = "/404.html";
-
-// Define MAX_TTL's in SECONDS for specific file extensions
 const MAX_TTL = {
   "/": 3600,
   html: 3600,
@@ -20,66 +21,6 @@ const MAX_TTL = {
   css: 86400
 };
 
-const CACHE_BLACKLIST = [
-  str => {
-    return !str.startsWith("http://localhost");
-  }
-];
-
-const SUPPORTED_METHODS = ["GET"];
-const SUPPORTED_URL_SCHEMES = ["http:", "https:"];
-
-/**
- * isBlackListed
- * @param {string} url
- * @returns {boolean}
- */
-function isBlacklisted(url) {
-  return CACHE_BLACKLIST.length > 0
-    ? !CACHE_BLACKLIST.filter(rule => {
-        if (typeof rule === "function") {
-          return !rule(url);
-        } else {
-          return false;
-        }
-      }).length
-    : false;
-}
-
-/**
- * getFileExtension
- * @param {string} url
- * @returns {string}
- */
-function getFileExtension(url) {
-  let extension = url
-    .split(".")
-    .reverse()[0]
-    .split("?")[0];
-  return extension.endsWith("/") ? "/" : extension;
-}
-
-/**
- * getTTL
- * @param {string} url
- */
-function getTTL(url) {
-  if (typeof url === "string") {
-    let extension = getFileExtension(url);
-    if (typeof MAX_TTL[extension] === "number") {
-      return MAX_TTL[extension];
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
-}
-
-/**
- * installServiceWorker
- * @returns {Promise}
- */
 function installServiceWorker() {
   return caches
     .open(CACHE_NAME)
@@ -91,10 +32,6 @@ function installServiceWorker() {
     });
 }
 
-/**
- * cleanupLegacyCache
- * @returns {Promise}
- */
 function cleanupLegacyCache() {
   let currentCaches = [CACHE_NAME];
   return new Promise((resolve, reject) => {
@@ -128,6 +65,46 @@ function cleanupLegacyCache() {
   });
 }
 
+function isCachedResponseExpired(response, ttl) {
+  const date = getRequestDateFromCachedResponse(response);
+  if (!date) {
+    return false;
+  }
+  let age = parseInt((new Date().getTime() - date.getTime()) / 1000);
+  return ttl && age > ttl;
+}
+
+function getRequestDateFromCachedResponse(response) {
+  let headers = response.headers.entries();
+  for (let pair of headers) {
+    if (pair[0] === "date") {
+      return new Date(pair[1]);
+    }
+  }
+  return null;
+}
+
+function getTTL(url) {
+  if (typeof url === "string") {
+    let extension = getFileExtension(url);
+    if (typeof MAX_TTL[extension] === "number") {
+      return MAX_TTL[extension];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+}
+
+function getFileExtension(url) {
+  let extension = url
+    .split(".")
+    .reverse()[0]
+    .split("?")[0];
+  return extension.endsWith("/") ? "/" : extension;
+}
+
 self.addEventListener("install", event => {
   event.waitUntil(Promise.all([installServiceWorker(), self.skipWaiting()]));
 });
@@ -149,67 +126,6 @@ function defaultResponse() {
   return caches.match(OFFLINE_PAGE);
 }
 
-function isCachedResponseExpired(response, ttl) {
-  const date = getRequestDateFromCachedResponse(response);
-  if (!date) {
-    return false;
-  }
-  let age = parseInt((new Date().getTime() - date.getTime()) / 1000);
-  return ttl && age > ttl;
-}
-
-function getRequestDateFromCachedResponse(response) {
-  let headers = response.headers.entries();
-  for (let pair of headers) {
-    if (pair[0] === "date") {
-      return new Date(pair[1]);
-    }
-  }
-  return null;
-}
-
-function getFromCache(request, url) {
-  return caches.open(CACHE_NAME).then(cache => {
-    return cache.match(request).then(cachedResponse => {
-      cachedResponseIsValid = cachedResponse && !isCachedResponseExpired(cachedResponse, getTTL(url))
-      if (cachedResponseIsValid) {
-        console.log(`retrieved cached response for: ${url}`);
-        return cachedResponse;
-      }
-      else {
-        console.log(`cache contains no-match for: ${url}`);
-        return fromNetwork(request, url)
-          .then(response => {
-            updateCache(request, url, response.clone())
-              .then(() => {
-                console.log(`completed fetch event for: ${url}`);
-                return response;
-              })
-              .catch(defaultResponse);
-          })
-          .catch(defaultResponse);
-      }
-    });
-  });
-}
-
-function updateCache(request, url, response) {
-  console.log(`adding cached response: ${url}`);
-  return caches.open(CACHE_NAME).then(cache => cache.put(request, response));
-}
-
-function fromNetwork(request, url) {
-  console.log(`requesting ${url} from network...`);
-  return fetch(request).then(response => {
-    if (!response || response.status >= 400) {
-      console.log(`fetch-error: CODE: ${response.status} (${url})`);
-      return Promise.reject(`fetch-error: CODE: ${response.status} (${url})`);
-    }
-    console.log(`successfully retrieved ${url}`);
-    return response;
-  });
-}
-
 self.addEventListener("fetch", function fetchHandler(event) {
   const { request } = event;
   const url = new URL(request.url);
@@ -220,5 +136,31 @@ self.addEventListener("fetch", function fetchHandler(event) {
     return Promise.reject(`unsupported-url-scheme: ${url}`);
   }
   console.log(`intercepted fetch event for: ${url}`)
-  event.respondWith(getFromCache(request, url).catch(defaultResponse));
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse && !isCachedResponseExpired(cachedResponse, getTTL(url))) {
+      console.log(`retrieved cached response for: ${url}`);
+      console.log(`completed fetch event for: ${url}`);
+      return cachedResponse;
+    }
+    if (cachedResponse) {
+      console.log(`cached response for ${url} is expired`)
+    }
+    else {
+      console.log(`cache contains no-match for: ${url}`);
+    }
+    console.log(`requesting ${url} from network...`);
+    const response = await fetch(event.request);
+    if (!response || response.status !== 200 || response.type !== 'basic') {
+      console.log(`fetch-error: ${response.type} CODE: ${response.status} (${url})`);
+      console.log(`completed fetch event for: ${url}`);
+      return response;
+    }
+    console.log(`successfully retrieved: ${url}`);
+    console.log(`adding response to cache: ${url}`);
+    await cache.put(event.request, response.clone());
+    console.log(`completed fetch event for: ${url}`);
+    return response;
+  })());
 });
