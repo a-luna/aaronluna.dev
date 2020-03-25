7d3f41fd96137a15e079e80368f9c908380ee6f5
Aaron Luna <admin@aaronluna.dev>
2020-03-25 14:36:18 -0700

    updated category widget

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,41 @@
 function addRelNoOpenerToExternalLinks(a) {
   if (a.getAttribute("target").startsWith("_blank")) {
-    a.setAttribute("rel", "noopener");
+    a.setAttribute("rel", "noopener")
   }
 }
 
 function addTargetBlankToExternalLinks(a) {
   if (!a.getAttribute("href").startsWith("/")) {
-    a.setAttribute("target", "_blank");
-    a.setAttribute("rel", "noopener");
+    a.setAttribute("target", "_blank")
+    a.setAttribute("rel", "noopener")
   }
 }
 
 function createToggle(accordion) {
-  const accordianSection = accordion.querySelector("section");
-  const button = accordion.querySelector(".accordion-button");
-  button.addEventListener("click", () => toggleAccordion(accordianSection));
+  const accordianSection = accordion.querySelector("section")
+  const button = accordion.querySelector(".accordion-button")
+  button.addEventListener("click", () => toggleAccordion(accordianSection))
 }
 
 function toggleAccordion(accordianSection) {
-  event.stopPropagation();
-  accordianSection.classList.toggle("ac_hidden");
+  event.stopPropagation()
+  accordianSection.classList.toggle("ac_hidden")
 }
 
-document
-  .querySelectorAll("a[target]")
-  .forEach(a => addRelNoOpenerToExternalLinks(a));
-document
-  .querySelectorAll(".note a, .alert a")
-  .forEach(a => addTargetBlankToExternalLinks(a));
-document
-  .querySelectorAll(".accordion")
-  .forEach(accordion => createToggle(accordion));
+function setSelectedCategory(pathname) {
+  const categorySelect = document.querySelector(".select-css-wrapper select")
+  categorySelect.value = pathname
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document
+    .querySelectorAll("a[target]")
+    .forEach(a => addRelNoOpenerToExternalLinks(a))
+  document
+    .querySelectorAll(".note a, .alert a")
+    .forEach(a => addTargetBlankToExternalLinks(a))
+  document
+    .querySelectorAll(".accordion")
+    .forEach(accordion => createToggle(accordion))
+  setSelectedCategory(window.location.pathname)
+});
